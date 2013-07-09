0457edabfc09e4dce5b2d0a8120a15f03796e352
Thomas J. Leeper <thosjleeper@gmail.com>
2013-07-09 15:37:48 +0200

    Added validation.test support Issue#1)

--- R/genericmturkr.R
+++ R/genericmturkr.R
@@ -1,19 +1,22 @@
 genericmturkr <-
 function (operation, parameters = NULL, keypair = credentials(), 
     print = TRUE, browser = FALSE, log.requests = TRUE, sandbox = FALSE, 
-    xml.parse = TRUE) 
+    xml.parse = TRUE, validattion.test = FALSE)
 {
     if (!is.null(keypair)) {
         keyid <- keypair[1]
         secret <- keypair[2]
     }
-    else stop("No keypair provided or 'credentials' object not stored")
+    else
+        stop("No keypair provided or 'credentials' object not stored")
     operation <- operation
     auth <- authenticate(operation, secret)
     GETparameters <- parameters
     request <- request(keyid, auth$operation, auth$signature, 
         auth$timestamp, GETparameters, browser = browser, log.requests = log.requests, 
-        sandbox = sandbox, xml.parse = xml.parse)
+        sandbox = sandbox, xml.parse = xml.parse, validation.test = validation.test)
+    if(validation.test)
+        message("Request URL:",request)
     if (request$valid == TRUE & print == TRUE) {
         message("Operation (", operation, ") Successful")
         return(request)
