1483643eab55f8a96904488ca87c06428c09073a
Thomas J. Leeper <thosjleeper@gmail.com>
2013-09-20 19:04:25 +0200

    clean up indentation

--- R/QuestionFormAnswersToDataFrame.R
+++ R/QuestionFormAnswersToDataFrame.R
@@ -14,20 +14,17 @@ function (xml = NULL, xml.parsed = NULL)
     questions <- xmlChildren(z)
     iterations <- total * length(questions)
     values <- as.data.frame(matrix(nrow = iterations, ncol = 10))
-    names(values) <- c("AssignmentId", "WorkerId", "HITId", "QuestionIdentifier", 
-        "FreeText", "SelectionIdentifier", "OtherSelectionField", 
-        "UploadedFileKey", "UploadedFileSizeInBytes", "Combined.Answers")
+    names(values) <- c(	"AssignmentId", "WorkerId", "HITId", "QuestionIdentifier", 
+        				"FreeText", "SelectionIdentifier", "OtherSelectionField", 
+        				"UploadedFileKey", "UploadedFileSizeInBytes", "Combined.Answers")
     j <- 1 	# j iterates rows of the original input data
 			# i iterates rows of the output dataframe
 	z <- 0 	# z iterates through answers within each row of the original data
 	for (i in 1:iterations) {
         z <- z + 1
-        values$AssignmentId[i] <- xmlValue(xpathApply(answers[[j]], 
-            "../AssignmentId")[[1]])
-        values$WorkerId[i] <- xmlValue(xpathApply(answers[[j]], 
-            "../WorkerId")[[1]])
-        values$HITId[i] <- xmlValue(xpathApply(answers[[j]], 
-            "../HITId")[[1]])
+        values$AssignmentId[i] <- xmlValue(xpathApply(answers[[j]], "../AssignmentId")[[1]])
+        values$WorkerId[i] <- xmlValue(xpathApply(answers[[j]], "../WorkerId")[[1]])
+        values$HITId[i] <- xmlValue(xpathApply(answers[[j]], "../HITId")[[1]])
         questions <- xmlChildren(extractQuestionFormAnswersElement(j))
         if (length(xmlChildren(questions[[z]])$QuestionIdentifier) == 1) 
             values$QuestionIdentifier[i] <- xmlValue(xmlChildren(questions[[z]])$QuestionIdentifier)
@@ -44,23 +41,24 @@ function (xml = NULL, xml.parsed = NULL)
         else if (length(xmlChildren(questions[[z]])$SelectionIdentifier) == 1) {
             values$SelectionIdentifier[i] <- xmlValue(xmlChildren(questions[[z]])$SelectionIdentifier)
             values$Combined.Answers[i] <- xmlValue(xmlChildren(questions[[z]])$SelectionIdentifier)
-            if (length(xmlChildren(questions[[1]])$OtherSelectionField) == 
-                1) {
+            if (length(xmlChildren(questions[[1]])$OtherSelectionField) == 1) {
                 multiple <- paste(values$SelectionIdentifier[i], 
-                  xmlValue(xmlChildren(questions[[z]])$OtherSelectionField), 
-                  sep = ";")
+                  			xmlValue(xmlChildren(questions[[z]])$OtherSelectionField), 
+                  			sep = ";")
                 values$Combined.Answers[i] <- multiple
             }
         }
         else if (length(xmlChildren(questions[[z]])$SelectionIdentifier) > 1) {
             multiple <- ""
             for (j in 1:length(xmlChildren(questions[[z]])$SelectionIdentifier)) {
-                multiple <- paste(multiple, xmlValue(xmlChildren(xmlChildren(questions[[z]])[i]$QuestionIdentifier)$text), 
-                  sep = ";")
+                multiple <- paste(multiple,
+                			xmlValue(xmlChildren(xmlChildren(questions[[z]])[i]$QuestionIdentifier)$text), 
+                  			sep = ";")
             }
             if (length(xmlChildren(questions[[z]])$OtherSelectionField) == 1) 
-                multiple <- paste(multiple, xmlValue(xmlChildren(questions[[z]])$OtherSelectionField), 
-                  sep = ";")
+                multiple <- paste(multiple,
+                			xmlValue(xmlChildren(questions[[z]])$OtherSelectionField), 
+                  			sep = ";")
             values$SelectionIdentifier[i] <- multiple
             values$Combined.Answers[i] <- multiple
         }
