92a64adc05bf57a14b32a8626a0bb86eca6fb245
leeper <thosjleeper@gmail.com>
2013-10-13 17:04:34 +0200

    add pmatch (#11) and eliminated loop (#3)

--- R/WorkerReport.R
+++ R/WorkerReport.R
@@ -3,22 +3,18 @@ function (worker, period = "LifeToDate", keypair = credentials(),
     log.requests = getOption('MTurkR.log'), sandbox = getOption('MTurkR.sandbox'),
     print = getOption('MTurkR.print')) 
 {
-    if (!period %in% c("OneDay", "SevenDays", "ThirtyDays", "LifeToDate")) 
-        stop("Period not valid")
+    if(is.na(pmatch(period,c("OneDay", "SevenDays", "ThirtyDays", "LifeToDate")))) 
+        stop("Period not valid. Must be 'OneDay', 'SevenDays', 'ThirtyDays', or 'LifeToDate'.   ")
     statistics <- c("NumberAssignmentsApproved", "NumberAssignmentsRejected", 
         "NumberKnownAnswersCorrect", "NumberKnownAnswersIncorrect", 
         "NumberKnownAnswersEvaluated", "NumberPluralityAnswersCorrect", 
         "NumberPluralityAnswersIncorrect", "NumberPluralityAnswersEvaluated", 
         "PercentAssignmentsApproved", "PercentAssignmentsRejected", 
         "PercentKnownAnswersCorrect", "PercentPluralityAnswersCorrect")
-    z <- data.frame(matrix(nrow = length(statistics), ncol = 2))
-    names(z) <- c("Statistic", "Value")
-    z[, 1] <- statistics
-    for (i in 1:length(statistics)) {
-        z[i, 2] <- GetWorkerStatistic(worker, statistics[i], 
-            period = period, keypair = keypair, print = FALSE, 
-            log.requests = log.requests, sandbox = sandbox)
-    }
+    z <- data.frame(Statistic=statistics, Value =
+        sapply(statistics, function(i) GetWorkerStatistic(worker, i, 
+                period = period, keypair = keypair, print = FALSE, 
+                log.requests = log.requests, sandbox = sandbox)), row.names=1:length(statistics) )
     if (print == TRUE) 
         print(z)
     invisible(z)
