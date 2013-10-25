5315a4e21d9c5039704de90c708e17a5c75863e4
Thomas J. Leeper <thosjleeper@gmail.com>
2013-10-25 21:32:22 +0200

    gsub non-XML ampersands from layout values (closes #30)

--- R/GenerateHITLayoutParameter.R
+++ R/GenerateHITLayoutParameter.R
@@ -2,6 +2,7 @@ GenerateHITLayoutParameter <-
 function (names, values) {
     if(!length(names) == length(values)) 
         stop("length(names) must equal length(values)")
+    values <- gsub('&(?!(?:apos|quot|[gl]t|amp);|#)','', values, perl=TRUE)
     parameter <- ""
     for(i in 1:length(names)) {
         parameter <- paste(parameter, "&HITLayoutParameter.", i,
