27ab78605636a583aab2fc8c3178e8b92c2d394d
leeper <thosjleeper@gmail.com>
2014-04-23 20:34:21 +0200

    attempt #3 on BonusPaymentsToDataFrame

--- R/BonusPaymentsToDataFrame.R
+++ R/BonusPaymentsToDataFrame.R
@@ -2,22 +2,18 @@ BonusPaymentsToDataFrame <-
 function (xml = NULL, xml.parsed = NULL){
     if(!is.null(xml)) 
         xml.parsed <- xmlParse(xml)
-    bonus.xml <- xpathApply(xml.parsed, "//BonusPayment")
-    if(length(bonus.xml) > 0) {
-        bonuses <- setNames(data.frame(matrix(nrow = length(bonus.xml), ncol = 7)),
-                    c("AssignmentId", "WorkerId", "Amount", "CurrencyCode",
-                    "FormattedPrice", "Reason", "GrantTime"))
-        for(i in 1:length(bonus.xml)) {
-            bonuses[i, ] <- c(	xmlValue(xpathApply(xml.parsed, "//AssignmentId")[[i]]),
-								xmlValue(xpathApply(xml.parsed, "//WorkerId")[[i]]),
-								xmlValue(xpathApply(xml.parsed, "//Amount")[[i]]),
-								xmlValue(xpathApply(xml.parsed, "//CurrencyCode")[[i]]),
-								xmlValue(xpathApply(xml.parsed, "//FormattedPrice")[[i]]),
-								xmlValue(xpathApply(xml.parsed, "//Reason")[[i]]),
-								xmlValue(xpathApply(xml.parsed, "//GrantTime")[[i]]))
-        }
-        return(bonuses)
-    }
-    else
-		return(NULL)
+    out <- xpathApply(xml.parsed, "//BonusPayment", function(x){
+        children <- xmlChildren(x)
+        bonus <- xmlChildren(children$BonusAmount)
+        return(list(
+            AssignmentId = xmlValue(children$AssignmentId),
+            WorkerId = xmlValue(children$WorkerId),
+            Amount = xmlValue(bonus$Amount),
+            CurrencyCode = xmlValue(bonus$CurrencyCode),
+            FormattedPrice = xmlValue(bonus$FormattedPrice),
+            Reason = xmlValue(children$Reason),
+            GrantTime = xmlValue(children$GrantTime)
+        ))
+    })
+    return(out)
 }
