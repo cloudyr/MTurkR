430d82101dca579794c7d862fbb7a402e9ea36fc
leeper <thosjleeper@gmail.com>
2014-04-25 19:12:29 +0200

    update QualificationTypesToDataFrame (#3)

--- R/QualificationTypesToDataFrame.R
+++ R/QualificationTypesToDataFrame.R
@@ -2,27 +2,22 @@ QualificationTypesToDataFrame <-
 function (xml = NULL, xml.parsed = NULL) {
     if(!is.null(xml)) 
         xml.parsed = xmlParse(xml)
-    total <- length(xpathApply(xml.parsed, "//QualificationTypeId"))
-    quals <- as.data.frame(matrix(nrow = total, ncol = 13))
-    names(quals) <- c("QualificationTypeId", "CreationTime", 
-        "Name", "Description", "Keywords", "QualificationTypeStatus", 
-        "AutoGranted", "AutoGrantedValue", "IsRequestable", "RetryDelayInSeconds", 
-        "TestDurationInSeconds", "Test", "AnswerKey")
-    for(i in 1:total) {
-        q <- xpathApply(xml.parsed, "//QualificationType")[[i]]
-        quals[i, 1] <- xmlValue(xmlChildren(q)$QualificationTypeId)
-        quals[i, 2] <- xmlValue(xmlChildren(q)$CreationTime)
-        quals[i, 3] <- xmlValue(xmlChildren(q)$Name)
-        quals[i, 4] <- xmlValue(xmlChildren(q)$Description)
-        quals[i, 5] <- xmlValue(xmlChildren(q)$Keywords)
-        quals[i, 6] <- xmlValue(xmlChildren(q)$QualificationTypeStatus)
-        quals[i, 7] <- xmlValue(xmlChildren(q)$AutoGranted)
-        quals[i, 8] <- xmlValue(xmlChildren(q)$AutoGrantedValue)
-        quals[i, 9] <- xmlValue(xmlChildren(q)$IsRequestable)
-        quals[i, 10] <- xmlValue(xmlChildren(q)$RetryDelayInSeconds)
-        quals[i, 11] <- xmlValue(xmlChildren(q)$TestDurationInSeconds)
-        quals[i, 12] <- xmlValue(xmlChildren(q)$Test)
-        quals[i, 13] <- xmlValue(xmlChildren(q)$AnswerKey)
-    }
-    return(quals)
+    out <- xpathApply(xml.parsed, "//QualificationType", function(x){
+        return(list(
+            QualificationTypeId = xmlValue(xmlChildren(x)$QualificationTypeId),
+            CreationTime = xmlValue(xmlChildren(x)$CreationTime),
+            Name = xmlValue(xmlChildren(x)$Name),
+            Description = xmlValue(xmlChildren(x)$Description),
+            Keywords = xmlValue(xmlChildren(x)$Keywords),
+            QualificationTypeStatus = xmlValue(xmlChildren(x)$QualificationTypeStatus),
+            AutoGranted = xmlValue(xmlChildren(x)$AutoGranted),
+            AutoGrantedValue = xmlValue(xmlChildren(x)$AutoGrantedValue),
+            IsRequestable = xmlValue(xmlChildren(x)$IsRequestable),
+            RetryDelayInSeconds = xmlValue(xmlChildren(x)$RetryDelayInSeconds),
+            TestDurationInSeconds = xmlValue(xmlChildren(x)$TestDurationInSeconds),
+            Test = xmlValue(xmlChildren(x)$Test),
+            AnswerKey = xmlValue(xmlChildren(x)$AnswerKey)
+        ))
+    })
+    return(do.call(rbind.data.frame,out))
 }
