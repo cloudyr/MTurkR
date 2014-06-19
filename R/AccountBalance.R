ff27fce1edead21302d49f094ce4b9c2f2e7020e
leeper <thosjleeper@gmail.com>
2014-06-19 13:18:51 +0200

    fix AccountBalance return

--- R/AccountBalance.R
+++ R/AccountBalance.R
@@ -8,15 +8,15 @@ function(verbose = getOption('MTurkR.verbose'), ...)
         verbose <- list(...)$print
     operation <- "GetAccountBalance"
     request <- request(operation, verbose = verbose, ...)
-    if(!is.null(request$valid))
+    if(is.null(request$valid) || !request$valid) {
         return(request)
-    else if(request$valid) {
+    } else if(request$valid) {
         balance <- strsplit(strsplit(request$xml, "<Amount>")[[1]][2], "</Amount>")[[1]][1]
         balanceformatted <- strsplit(strsplit(request$xml, 
             "<FormattedPrice>")[[1]][2], "</FormattedPrice>")[[1]][1]
-        if(verbose == TRUE)
-            message(paste("Balance: ", balanceformatted, "\n", sep = ""))
         request$balance <- balance
+        if(verbose)
+            message(paste("Balance: ", balanceformatted, "\n", sep = ""))
+        return(invisible(request))
     }
-    return(request)
 }
