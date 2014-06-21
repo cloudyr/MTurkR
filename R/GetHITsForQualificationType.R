dfaa7716b00fe836471073435fa0c316cbc85958
Thomas J. Leeper <thosjleeper@gmail.com>
2014-06-21 10:09:14 +0200

    misc code cleanup

--- R/GetHITsForQualificationType.R
+++ R/GetHITsForQualificationType.R
@@ -16,30 +16,31 @@ function (qual, response.group = NULL, return.all = TRUE, pagenumber = 1,
     if(as.numeric(pagenumber) < 1) 
         stop("'pagenumber' must be > 1")
     GETparameters <- paste("&QualificationTypeId=", qual, "&PageSize=", 
-        pagesize, sep = "")
+                           pagesize, sep = "")
     if(!is.null(response.group)) {
-        #if(!response.group %in% c("Minimal", "HITQuestion", "HITDetail", "HITAssignmentSummary")) 
         if(!response.group %in% c("Minimal", "Request")) 
             stop("ResponseGroup must be in c(Minimal,Request)")
-        if(length(response.group) == 1) 
-            GETparameters <- paste(GETparameters, "&ResponseGroup=", response.group, sep = "")
-        else {
+        if(length(response.group) == 1) {
+            GETparameters <- paste(GETparameters, "&ResponseGroup=", 
+                                   response.group, sep = "")
+        } else {
             for(i in 1:length(response.group)) {
                 GETparameters <- paste(GETparameters, "&ResponseGroup", i - 1,
-                                        "=", response.group[i], sep = "")
+                                       "=", response.group[i], sep = "")
             }
         }
     }
     batch <- function(pagenumber) {
         GETiteration <- paste(GETparameters, "&PageNumber=", 
-            pagenumber, sep = "")
-        batch <- request(operation, GETparameters = GETiteration, ...)
+                              pagenumber, sep = "")
+        batch <- request(operation, GETparameters = GETiteration, 
+                         verbose = verbose, ...)
         if(is.null(batch$valid))
             return(batch)
         batch$total <- as.numeric(strsplit(strsplit(batch$xml, 
             "<TotalNumResults>")[[1]][2], "</TotalNumResults>")[[1]][1])
         batch$batch.total <- length(xpathApply(xmlParse(batch$xml), "//HIT"))
-        if(return.hit.dataframe == TRUE) {
+        if(return.hit.dataframe) {
             if(batch$total > 0) {
                 hitlist <- as.data.frame.HITs(xml.parsed = xmlParse(batch$xml))
                 batch$HITs <- hitlist$HITs
@@ -58,8 +59,8 @@ function (qual, response.group = NULL, return.all = TRUE, pagenumber = 1,
         request$request.id <- c(request$request.id, nextbatch$request.id)
         request$valid <- c(request$valid, nextbatch$valid)
         request$xml.response <- c(request$xml, nextbatch$xml)
-        if(request$valid == TRUE) {
-            if(return.hit.dataframe == TRUE) {
+        if(request$valid) {
+            if(return.hit.dataframe) {
                 request$HITs <- rbind(request$HITs, nextbatch$HITs)
                 request$QualificationRequirements <- rbind(request$QualificationRequirements, 
                     nextbatch$QualificationRequirements)
