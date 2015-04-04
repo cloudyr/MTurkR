1e19d7ac71a3f81fa341a8e0ef25e1828113bbee
leeper <thosjleeper@gmail.com>
2015-04-04 14:26:51 +0200

    do not overwrite options on load (closes #83)

--- R/onLoad.r
+++ R/onLoad.r
@@ -1,10 +1,16 @@
 .onLoad <- function(libname, pkgname){
-    options(MTurkR.logdir = getwd())    # MTurkRlog.tsv directory
-    options(MTurkR.sandbox = FALSE)     # sandbox logical
-    options(MTurkR.verbose = TRUE)      # print logical
-    options(MTurkR.browser = FALSE)     # browser logical
-    options(MTurkR.log = TRUE)          # log logical
-    options(MTurkR.test = FALSE)        # validation.test logical
+    if(is.null(getOption("MTurkR.logdir"))
+        options(MTurkR.logdir = getwd())    # MTurkRlog.tsv directory
+    if(is.null(getOption("MTurkR.sandbox"))
+        options(MTurkR.sandbox = FALSE)     # sandbox logical
+    if(is.null(getOption("MTurkR.verbose"))
+        options(MTurkR.verbose = TRUE)      # print logical
+    if(is.null(getOption("MTurkR.browser"))
+        options(MTurkR.browser = FALSE)     # browser logical
+    if(is.null(getOption("MTurkR.log"))
+        options(MTurkR.log = TRUE)          # log logical
+    if(is.null(getOption("MTurkR.test"))
+        options(MTurkR.test = FALSE)        # validation.test logical
     a <- Sys.getenv("AWS_ACCESS_KEY_ID")
     s <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
     if(a != "" & s != "") {
