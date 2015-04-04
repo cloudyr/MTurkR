2505ac4a5074ed3365a5a3ccf3ff503ea0a3caf4
Thomas J. Leeper <thosjleeper@gmail.com>
2015-04-04 16:26:25 +0200

    close brackets

--- R/onLoad.r
+++ R/onLoad.r
@@ -1,15 +1,15 @@
 .onLoad <- function(libname, pkgname){
-    if(is.null(getOption("MTurkR.logdir"))
+    if(is.null(getOption("MTurkR.logdir")))
         options(MTurkR.logdir = getwd())    # MTurkRlog.tsv directory
-    if(is.null(getOption("MTurkR.sandbox"))
+    if(is.null(getOption("MTurkR.sandbox")))
         options(MTurkR.sandbox = FALSE)     # sandbox logical
-    if(is.null(getOption("MTurkR.verbose"))
+    if(is.null(getOption("MTurkR.verbose")))
         options(MTurkR.verbose = TRUE)      # print logical
-    if(is.null(getOption("MTurkR.browser"))
+    if(is.null(getOption("MTurkR.browser")))
         options(MTurkR.browser = FALSE)     # browser logical
-    if(is.null(getOption("MTurkR.log"))
+    if(is.null(getOption("MTurkR.log")))
         options(MTurkR.log = TRUE)          # log logical
-    if(is.null(getOption("MTurkR.test"))
+    if(is.null(getOption("MTurkR.test")))
         options(MTurkR.test = FALSE)        # validation.test logical
     a <- Sys.getenv("AWS_ACCESS_KEY_ID")
     s <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
