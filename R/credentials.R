7a5ad77b3f88ccdf4efe9fef84d83c121464190d
Thomas J. Leeper <thosjleeper@gmail.com>
2015-11-11 17:43:24 +0000

    deprecate credentials and clean namespace (closes #96)

--- R/credentials.R
+++ R/credentials.R
@@ -1,4 +1,5 @@
 credentials <- function(keypair=NULL){
+    message("credentials() is being deprecated!\nInstead, specify AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables.")
     if(!is.null(keypair))
         options(MTurkR.keypair = keypair)
     else
