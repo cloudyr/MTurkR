4261368c653072ce925b56648a14b63b0643546a
leeper <thosjleeper@gmail.com>
2016-04-10 19:50:47 +0100

    add Sys.sleep() b/w tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -6,14 +6,21 @@ test_that("Account Balance Check", {
     expect_true(is.numeric(SufficientFunds(amount = 1, assignments = 1, hits = 1, bonus.ct = 1, bonus.amount = 1)$Total))
 })
 
+Sys.sleep(1)
+
 test_that("RequesterReport", {
     expect_true(is.data.frame(RequesterReport()))
     expect_true(is.data.frame(WorkerReport("A1RO9UJNWXMU65", period = "OneDay", sandbox = TRUE)))
 })
+
+Sys.sleep(1)
+
 test_that("RequesterReport", {
     expect_true(is.data.frame(WorkerReport("A1RO9UJNWXMU65", sandbox = TRUE)))
     expect_true(is.data.frame(WorkerReport("A1RO9UJNWXMU65", period = "OneDay", sandbox = TRUE)))
 })
+
+Sys.sleep(1)
 
 
 context("Workers")
@@ -21,16 +28,25 @@ context("Workers")
 test_that("GetBlockedWorkers", {
     expect_true(is.data.frame(GetBlockedWorkers(sandbox = TRUE)))
 })
+
+
+Sys.sleep(1)
+
 test_that("GrantBonus", {
     #expect_true(is.data.frame(GrantBonus("A1RO9UJNWXMU65", amount = 0.01, sandbox = TRUE)))
     expect_error(GrantBonus(workers = "A1RO9UJNWXMU65", sandbox = TRUE))
 })
+
+Sys.sleep(1)
+
 test_that("ContactWorkers", {
     expect_error(ContactWorkers(workers = "A1RO9UJNWXMU65", sandbox = TRUE))
     expect_error(ContactWorkers(subjects = "this is a test", sandbox = TRUE))
     expect_error(ContactWorkers(msgs = "this is a test", sandbox = TRUE))
     expect_true(is.data.frame(ContactWorkers("MTurkR Test", "This is a test", "A1RO9UJNWXMU65", sandbox = TRUE)))
 })
+
+Sys.sleep(1)
 
 
 
@@ -42,7 +58,6 @@ test_that("Question Formats", {
     f <- system.file("templates/htmlquestion1.xml", package = "MTurkR")
     expect_true(inherits(GenerateHTMLQuestion(file=f), "HTMLQuestion"))
 })
-
 
 r <- try(RegisterHITType(title = "Example HITType",
                      description = "empty",
@@ -67,10 +82,16 @@ if (!inherits(r, "try-error")) {
         s2 <- SetHITTypeNotification(hit.type = r$HITTypeId, notification = n2, active = TRUE, sandbox = TRUE)
         expect_true(is.data.frame(s1))
         expect_true(is.data.frame(s2))
+        
+        Sys.sleep(1)
+
         s3 <- SetHITTypeNotification(hit.type = r$HITTypeId, notification = n1, active = FALSE, sandbox = TRUE)
         s4 <- SetHITTypeNotification(hit.type = r$HITTypeId, notification = n2, active = FALSE, sandbox = TRUE)
         expect_true(is.data.frame(s3))
         expect_true(is.data.frame(s4))
+        
+        Sys.sleep(1)
+
         rm(n1)
         rm(n2)
         rm(s1)
@@ -92,21 +113,29 @@ if (!inherits(h, "try-error")) {
         expect_true(as.character(r$HITTypeId[1]) == as.character(h$HITTypeId[1]))
     })
 
+    Sys.sleep(1)
+    
     test_that("ExtendHIT (assignments)", {
         e1 <- ExtendHIT(hit = h$HITId, add.assignments = 1, sandbox = TRUE)
         expect_true(nrow(e1) == 1)
     })
 
+    Sys.sleep(1)
+    
     test_that("ExtendHIT (seconds)", {
         e1 <- ExtendHIT(hit = h$HITId, add.seconds = 3600, sandbox = TRUE)
         expect_true(nrow(e1) == 1)
     })
 
+    Sys.sleep(1)
+    
     test_that("ExpireHIT", {
         e2 <- ExpireHIT(hit = h$HITId, sandbox = TRUE)
         expect_true(nrow(e2) == 1)
     })
 
+    Sys.sleep(1)
+    
     test_that("ChangeHITType", {
         r2 <- RegisterHITType(title = "Example HITType 2",
                               description = "empty",
@@ -118,36 +147,55 @@ if (!inherits(h, "try-error")) {
         expect_true(nrow(ch) == 1)
     })
 
+    Sys.sleep(1)
+    
     test_that("SearchHITs", {
         expect_true(is.data.frame(SearchHITs(sandbox = TRUE)$HITs))
     })
 
+    Sys.sleep(1)
+    
     test_that("GetReviewableHITs", {
         expect_true(is.data.frame(GetReviewableHITs(sandbox = TRUE)))
     })
 
+    Sys.sleep(1)
+    
     test_that("GetAssignments", {
         a <- GetAssignments(hit = h$HITId, sandbox = TRUE)
         expect_true(nrow(a) == 0)
     })
 
+    Sys.sleep(1)
+    
     test_that("GetHIT", {
         expect_true(is.data.frame(GetHIT(hit = h$HITId, sandbox = TRUE)$HITs))
     })
 
+    Sys.sleep(1)
+    
     test_that("DisposeHIT", {
         d <- DisposeHIT(hit = h$HITId, sandbox = TRUE)
         expect_true(nrow(d) == 1)
     })
+
+    Sys.sleep(1)
+    
 }
 
 
 context("QualificationTypes")
 
 sqt <- SearchQualificationTypes(return.all = TRUE, sandbox = TRUE)
+
+Sys.sleep(1)
+    
 if ("Example Qualification for Tests" %in% sqt$Name) {
     DisposeQualificationType(sqt$QualificationTypeId[which(sqt$Name == "Example Qualification for Tests")], sandbox = TRUE)
 }
+
+Sys.sleep(1)
+    
 q1 <- try(CreateQualificationType(name = "Example Qualification for Tests",
                               description = "empty",
                               status = "Active",
@@ -159,38 +207,53 @@ if (!inherits(q1, "try-error")) {
         expect_true(nrow(q1) == 1)
     })
 
+    Sys.sleep(1)
+    
     test_that("GetHITsForQualificationType", {
         expect_true(is.data.frame(GetHITsForQualificationType(qual = q1$QualificationTypeId, sandbox = TRUE)$HITs))
         expect_error(GetHITsForQualificationType(sandbox = TRUE))
     })
 
+    Sys.sleep(1)
+    
     test_that("UpdateQualificationType", {
         u <- UpdateQualificationType(q1$QualificationTypeId, 
                                      description = "new", 
                                      sandbox = TRUE)
         expect_true(is.data.frame(u))
+        Sys.sleep(1)
         g <- GetQualificationType(q1$QualificationTypeId, sandbox = TRUE)
         expect_true(g$Description[1] == "new")
         expect_error(GetQualificationType(sandbox = TRUE))
     })
 
+    Sys.sleep(1)
+    
     test_that("AssignQualification", {
         expect_true(is.data.frame(AssignQualification(q1$QualificationTypeId, workers = "A1RO9UJNWXMU65", sandbox = TRUE)))
     })
 
+    Sys.sleep(1)
+    
     test_that("UpdateQualificationScore", {
         expect_true(is.data.frame(UpdateQualificationScore(q1$QualificationTypeId, workers = "A1RO9UJNWXMU65", values = 2, sandbox = TRUE)))
         expect_true(is.data.frame(UpdateQualificationScore(q1$QualificationTypeId, workers = "A1RO9UJNWXMU65", increment = 1, sandbox = TRUE)))
     })
 
+    Sys.sleep(1)
+    
     test_that("RevokeQualification", {
         expect_true(is.data.frame(RevokeQualification(q1$QualificationTypeId, worker = "A1RO9UJNWXMU65", sandbox = TRUE)))
     })
 
+    Sys.sleep(1)
+    
     test_that("SearchQualificationTypes", {
         expect_true(is.data.frame(SearchQualificationTypes(sandbox = TRUE)))
     })
 
+    Sys.sleep(1)
+    
     test_that("DisposeQualificationType", {
         dis <- DisposeQualificationType(q1$QualificationTypeId, sandbox = TRUE)
         expect_true(nrow(dis) == 1)
